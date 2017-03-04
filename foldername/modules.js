9d58a53b9f12320e685040dfc5c4076c45f569c5
Riza Fahmi <rizafahmi@gmail.com>
2017-03-04 18:47:06 +0700

    Calling back after executing methods

---
+++ foldername/modules.js
@@ -0,0 +1,7 @@
+const methods = () => {
+  return 'Welcome to Stomatic Clot. You\'re on foldername/modules/methods.'
+}
+
+module.exports = {
+  methods
+}
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const http = require('http')
 
-const dispatch = (url) => {
+const dispatch = (url, cb) => {
   const [path, query] = url.split('?')
-  const [_empty, moduleName, methodName] = path.split('/')
-  console.log(moduleName, methodName)
+  const [_empty, folderName, moduleName, methodName] = path.split('/')
+  const module = require(`./${folderName}/${moduleName}`)
+  cb(module.methods())
 }
 
 const httpHandler = (request, response) => {
   // Requests
   // const method = request.method
   const url = request.url
-  if (url !== '/favicon.ico') {
-    dispatch(url)
-  }
 
   // Response
   response.setHeader('Content-Type', 'application/json')
   response.setHeader('X-Powered-By', 'Stomatic Clot')
-  response.write('Welcome to Stomatic Clot')
-  response.end()
+  if (url !== '/favicon.ico') {
+    dispatch(url, (content) => {
+      response.write(content)
+      response.end()
+    })
+  }
 }
 
 const server = http.createServer(httpHandler)
